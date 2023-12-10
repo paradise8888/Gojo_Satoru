da88e837c5a6dd5ac750451532aa2055ac852add
paradise8888 <121363647+paradise8888@users.noreply.github.com>
2023-12-11 06:43:43 +0800

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -41,18 +41,18 @@ class Config:
     AuDD_API = config("AuDD_API",default=None)
     RMBG_API = config("RMBG_API",default=None)
     DB_URI = config("DB_URI", default="")
-    DB_NAME = config("DB_NAME", default="gojo_satarou")
+    DB_NAME = config("DB_NAME", default="desatuak")
     BDB_URI = config("BDB_URI",default=None)
     NO_LOAD = config("NO_LOAD", default="").split()
     PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="XV_SUPPORT")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="LocalMemo")
     WORKERS = int(config("WORKERS", default=16))
     TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
-    BOT_USERNAME = ""
-    BOT_ID = ""
-    BOT_NAME = ""
-    owner_username = ""
+    BOT_USERNAME = "Spamxmoan_bot"
+    BOT_ID = "6654681821"
+    BOT_NAME = "SpamxMoan"
+    owner_username = "xexenid"
 
 
 class Development:
@@ -60,22 +60,22 @@ class Development:
 
     # Fill in these vars if you want to use Traditional method of deploying
     LOGGER = True
-    BOT_TOKEN = "YOUR BOT_TOKEN"
-    API_ID = 12345  # Your APP_ID from Telegram
-    API_HASH = "YOUR API HASH"  # Your APP_HASH from Telegram
-    OWNER_ID = 1344569458  # Your telegram user id defult to mine
-    MESSAGE_DUMP = -100845454887  # Your Private Group ID for logs
+    BOT_TOKEN = "6654681821:AAEW1l-PsFFYctn0Ruh8Srd8zcCXa0956tg"
+    API_ID = 27125828  # Your APP_ID from Telegram
+    API_HASH = "0220766f8c8ca1a28b12a9356ada0925"  # Your APP_HASH from Telegram
+    OWNER_ID = 1563707058 # Your telegram user id defult to mine
+    MESSAGE_DUMP = -1001683506649 # Your Private Group ID for logs
     DEV_USERS = []
     SUDO_USERS = []
     WHITELIST_USERS = []
-    DB_URI = ""  # Your mongo DB URI
-    DB_NAME = ""  # Your DB name
+    DB_URI = "mongodb+srv://desatuak:bimakeren321@cluster0.od1yy2z.mongodb.net/?retryWrites=true&w=majority"  # Your mongo DB URI
+    DB_NAME = "desatuak"  # Your DB name
     NO_LOAD = []
     GENIUS_API_TOKEN = ""
     RMBG_API = ""
     PREFIX_HANDLER = ["!", "/","$"]
-    SUPPORT_GROUP = "SUPPORT_GROUP"
-    SUPPORT_CHANNEL = "SUPPORT_CHANNEL"
+    SUPPORT_GROUP = "XV_SUPPORT"
+    SUPPORT_CHANNEL = "LocalMemo"
     VERSION = "VERSION"
     TIME_ZONE = 'Asia/Kolkata'
     BDB_URI = ""
